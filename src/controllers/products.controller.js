bc2b3be82901466ddeceb65b4bbd1aed4ceaceae
Agustin Alvarez <lukee1708@outlook.com>
2025-03-05 22:51:58 -0300

    Product Router CON controllers

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,1 +1,33 @@
-    
+import { productManager } from '../manager/product.manager.js'               //Importamos productManager desde manager para poder manejar los productos
+
+export const getAll = async (req, res) => {                      //Para leer la informacion de todos los productos tenemos que crear un lugar a donde IR a leerlos
+    try{
+        const response = await productManager.getAll();         //Llamo al metodo getAll creado en productManager para mostrar todos los productos
+        res.status(200).json(response)                            //Retorna los objetos 
+    } catch {
+        console.log("Error");                                   //Si falla nos devolvera el error
+        res.status(400).json({message: error.message });
+    }
+}
+
+export const create = async (req, res) => {                      //Metodo para crear productos
+    try{
+        const response = await productManager.create(req.body);         //Llamo al metodo create para realizar la creacion de un producto nuevo mediante lo q ingresemos
+        res.status(201).json(response)                            //Retorna el objeto nuevo
+    } catch {
+        console.log("Error");                                   //Si falla nos devolvera el error
+        res.status(400).json({message: error.message });
+    }
+}
+
+export const getById =  async (req, res) => {                      //Metodo para encontrar por id un producto especifico
+    try{
+        const {id} = req.params                                     //El id va a ser el que nosotros pasemos por el REQ
+        const response = await productManager.getById(id);                  //Llamo al metodo getById para pasar por parametro el producto que buscamos y q este lo encuentre
+        if (!response) return res.status(404).json({ message: 'Product not found'});        //Si no existe el objeto nos devolvera el error
+        res.status(200).json(response)                            //Retorna el objeto del id 
+    } catch {
+        console.log("Error");                                   //Si falla nos devolvera el error
+        res.status(400).json({message: error.message });
+    }
+}
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,40 +1,10 @@
 import { Router } from "express";               //Nos permitira manejar los endpoints del servidor por fuera de app.js
-import { productManager } from '../manager/product.manager.js'               //Importamos productManager desde manager para poder manejar los productos
+import * as controllers from '../controllers/products.controller.js'        //Importamos todos los controladores de productos para dejar mas limpia esta seccion
 
 const router = Router();                        //instanciamos router
 
-router.get('/', async (req, res) => {                      //Para leer la informacion de todos los productos tenemos que crear un lugar a donde IR a leerlos
-    try{
-        const response = await productManager.getAll();         //Llamo al metodo getAll creado en productManager para mostrar todos los productos
-        res.status(200).json(response)                            //Retorna los objetos 
-    } catch {
-        console.log("Error");                                   //Si falla nos devolvera el error
-        res.status(400).json({message: error.message });
-    }
-
-})
-
-router.post('/', async (req, res) => {                      //Metodo para crear productos
-    try{
-        const response = await productManager.create(req.body);         //Llamo al metodo create para realizar la creacion de un producto nuevo mediante lo q ingresemos
-        res.status(201).json(response)                            //Retorna el objeto nuevo
-    } catch {
-        console.log("Error");                                   //Si falla nos devolvera el error
-        res.status(400).json({message: error.message });
-    }
-})
-
-router.get('/:id', async (req, res) => {                      //Metodo para encontrar por id un producto especifico
-    try{
-        const {id} = req.params                                     //El id va a ser el que nosotros pasemos por el REQ
-        const response = await productManager.getById(id);                  //Llamo al metodo getById para pasar por parametro el producto que buscamos y q este lo encuentre
-        if (!response) return res.status(404).json({ message: 'Product not found'});        //Si no existe el objeto nos devolvera el error
-        res.status(200).json(response)                            //Retorna el objeto del id 
-    } catch {
-        console.log("Error");                                   //Si falla nos devolvera el error
-        res.status(400).json({message: error.message });
-    }
-
-})
+router.get('/', controllers.getAll);            //Mediante el controllers nos retorna todos los productos
+router.post('/', controllers.create);           //Mediante el controllers creamos un producto nuevo
+router.get('/:id', controllers.getById);        //Mediante el controllers nos retorna el producto introducido por id
 
 export default router;                          //Exportamos para poder usarlos en app.js
