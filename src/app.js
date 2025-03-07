475b17bd57d38569e48ce8c865a7ee91ae18b872
Agustin Alvarez <lukee1708@outlook.com>
2025-03-06 22:35:22 -0300

    Cookies C2

--- src/app.js
+++ src/app.js
@@ -1,24 +1,49 @@
-import express from "express";
+import express from "express";                                       //Importamos el modulo de express
+import cookieParser from "cookie-parser";                            //Importamos el modulo de cookies
+import 'dotenv/config';                                              //Importamos el modulo de variables de entorno
 import productRouter from "./routes/products.router.js";             //Importamos todos los metodos manejables desde endpoints de productos
 import { connectDB } from "./config/mongoose.config.js";             //Importamos la funcion de conexion a la base de datos
-import { errorHandler } from "./middlewares/error.handler.js";
+import { errorHandler } from "./middlewares/error.handler.js";       //Importamos el manejador de errores
 
-const app = express();
-const PORT = 8080;
+const app = express();                                               //Creamos una instancia de express
+const PORT = 8080;                                                   //Puerto en el que se ejecutara el servidor
+const SECRET_KEY = process.env.SECRET_KEY;                           //Llave secreta para las cookies
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(SECRET_KEY));                                  //Usamos el modulo de cookies
+app.use(express.json());                                            //Usamos el modulo de json
+app.use(express.urlencoded({ extended: true }));                    //Usamos el modulo de urlencoded
 
-app.get("/", (req, res) => {
-  //Pagina principal del sitio
+app.get("/", (req, res) => {                                        //Pagina principal
   res.send("hola Mundo");
 });
 
-app.use("/products", productRouter); //Pagina para usar todos los metodos de PRODUCTS
+app.get("/set-cookie", (req, res) => {                                        //Pagina para setear una cookie
+  res.cookie('idioma', 'ingles', {maxAge: 3000}).json({message: 'Cookie seteada'});          //res.cookie recibe clave y valor de la cookie
+});
 
-app.use(errorHandler);
+app.get("/set-signed-cookie", (req, res) => {                                        //Pagina para setear una cookie firmada
+  res.cookie('nombre', 'raul', {maxAge: 3000, signed: true, httpOnly: true}).json({message: 'Cookie seteada'});          //res.cookie recibe clave y valor de la cookie
+});
 
-connectDB();                         //Conexion a la base de datos
+app.get("/get-cookie", (req, res) => {                                        //Pagina para obtener una cookie
+  console.log(req.cookies);                                                   //req.cookies devuelve un objeto con todas las cookies
+  const {idioma} = req.cookies;                                               //Extraemos la cookie idioma
+  idioma === 'ingles' ? res.send({message: 'Hello'}) : res.send({message: 'Hola'});  //Si idioma es ingles, responde Ingles, sino Español
+  
+});
+
+app.get("/get-signed-cookie", (req, res) => {                                        //Pagina para obtener una cookie firmada
+  console.log(req.signedCookies);                                                   //req.cookies devuelve un objeto con todas las cookies
+  const {idioma} = req.cookies;                                               //Extraemos la cookie idioma
+  idioma === 'ingles' ? res.send({message: 'Hello'}) : res.send({message: 'Hola'});  //Si idioma es ingles, responde Ingles, sino Español
+  
+});
+
+app.use("/products", productRouter);                                //Pagina para usar todos los metodos de PRODUCTS
+
+app.use(errorHandler);                                              //Manejador de errores
+
+connectDB();                                                       //Conexion a la base de datos
 
 app.listen(PORT, () => {
   console.log(`Ejecutándose en http://localhost:${PORT}`);
--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -1,10 +1,17 @@
-import { connect } from "mongoose";                         //Importamos connect desde mongoose para realizar la conexion a nuestra base de datos
+import { connect, Types } from "mongoose";                         //Importamos connect desde mongoose para realizar la conexion a nuestra base de datos
+import 'dotenv/config';                                    //Importamos el modulo de variables de entorno
 
-export const connectDB = async () => {                      //Creamos una funcion asincrona para realizar la conexion a la base de datos
-    try{
-        await connect("mongodb+srv://agustin:1234@cluster0.jm6lw.mongodb.net/backend")        //Realizamos la conexion a la base de datos
-        console.log("Conectado A la Base De Datos");          //En caso de que la conexion sea exitosa, mostramos un mensaje
-    } catch (error) {                                       //En caso de que ocurra un error, lo mostramos en consola
-        throw new Error(error);                             //Lanzamos un error
+export const connectDB = async () => {                      //Funcion asincrona para conectar a la base de datos
+    const URL = process.env.MONGO_URL;                      //Obtenemos la URL de la base de datos desde las variables de entorno
+
+    try {                                                   //Intentamos conectar a la base de datos
+        await connect(URL);                                 //Usamos la funcion connect de mongoose para conectarnos a la base de datos
+        console.log("Conectado a MongoDB");                 //Si la conexion es exitosa, mostramos un mensaje en consola
+    } catch (error) {   
+        console.log("Error al conectar con MongoDB", error.message);            
     }
-} 
+};
+
+export const isValidID = (id) => {                          //Funcion para validar si un ID es valido
+    return Types.ObjectId.isValid(id);
+};
